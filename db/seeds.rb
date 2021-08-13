2759e7cf33d194ff4e0a1b0a8b1428667899ac6e
doorgesh neetye <neetyedoorgesh@gmail.com>
2021-08-13 08:58:13 +0400

    implemented images

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,16 @@ Trip.destroy_all
   longitude:"57.3082",
   latitude:"-20.4563",
   duration:"3 hr",
+  image_url:"https://res.cloudinary.com/trip-buddy/image/upload/v1628827854/trip%20buddy/sightseeing/le-morne-brabant_4_d9ilni.jpg",
   price: 700,
   stop_1:"Blue Marlin- For a more challenging trip",
   stop_1_lat:"-20.1897",
   stop_1_long:"57.5061",
+  stop_1_img_url:"https://res.cloudinary.com/trip-buddy/image/upload/v1628827977/trip%20buddy/sightseeing/paradis-beachcomber-golf_mtdytu.jpg",
   stop_2:"Slave Route Monument",
   stop_2_lat:"-20.4523",
   stop_2_long:"57.3143",
+  stop_2_img_url:"https://res.cloudinary.com/trip-buddy/image/upload/v1628828108/trip%20buddy/sightseeing/international-slave-route_fs3caq.jpg",
   choice:"day",
   category:"hiking"
   }
@@ -31,10 +34,12 @@ hiking_trip3 =
   longitude:"57.5552",
   latitude:"-20.1923",
   duration:"4 hr 30 min ",
+  image_url:"https://res.cloudinary.com/trip-buddy/image/upload/v1628828220/trip%20buddy/sightseeing/unnamed_tfmmm4.jpg",
   price: 1200,
   stop_1:"Malenga View Point Camping Area",
   stop_1_lat:"-20.1963",
   stop_1_long:"57.5606",
+  stop_1_img_url:"https://res.cloudinary.com/trip-buddy/image/upload/v1628828356/trip%20buddy/sightseeing/download_yrtn7q.jpg",
   choice:"day",
   category:"hiking"
 }
@@ -43,6 +48,7 @@ hiking_trip3 =
     puts "Created #{trip.name}"
   end
   puts "Finished creating hiking trips!"
+
 sight_seeing1 = {  
   name:"red-roofed Notre Dame Auxiliatrice",
   description: "Visit the Cap Malheureux which is a fishing village famous for the red-roofed Notre Dame Auxiliatrice.The tour will be conducted in a comfortable air-conditioned car with a professional driver. You will be accompanied by a guide throughout the tour who will tell you interesting stories and facts about the places.",
@@ -50,10 +56,12 @@ sight_seeing1 = {
   longitude:"57.6222",
   latitude:"-19.9866",
   duration:"1 hr",
+  image_url:"https://res.cloudinary.com/trip-buddy/image/upload/v1628680401/trip%20buddy/sightseeing/Church_gm4oon.jpg",
   price: 400,
   stop_1:"amigo restaurant- leading seafood venue in the north of the island,Renowned for its langouste.",
   stop_1_lat:"-19.9932",
   stop_1_long:"57.6139",
+  stop_1_img_url:"https://res.cloudinary.com/trip-buddy/image/upload/v1628829393/trip%20buddy/sightseeing/download_1_jadu2r.jpg",
   choice:"day",
   category:"sight-seeing"
 }
@@ -64,13 +72,16 @@ sight_seeing2 = {
   longitude:"57.50417",
   latitude:"-20.16139",
   duration:"2 hr 30 min ",
+  image_url:"https://res.cloudinary.com/trip-buddy/image/upload/v1628829497/trip%20buddy/sightseeing/History-Of-Citadel-Fort_o6p7ei.jpg",
   price: 200,
   stop_1:"Champs de Mars -home to the oldest racecourse in the Southern Hemisphere",
   stop_1_lat:"-20.1692",
   stop_1_long:"57.5102",
-  stop_2:"Gloria kebab",
+  stop_1_img_url:"https://res.cloudinary.com/trip-buddy/image/upload/v1628829563/trip%20buddy/sightseeing/horse-racing-in-mauritius-champ-de-mars-racecourse-521665_rqv1gq.jpg",
+  stop_2:"Gloria fast food",
   stop_2_lat:"-20.16357",
   stop_2_long:"57.5049828",
+  stop_2_img_url:"https://res.cloudinary.com/trip-buddy/image/upload/v1628830360/trip%20buddy/sightseeing/images_g9gniw.jpg",
   choice:"day",
   category:"sight-seeing"
 }
@@ -81,10 +92,12 @@ sight_seeing3 = {
   longitude:"57.50417",
   latitude:"-20.4180",
   duration:"4 hr 30 min ",
+  image_url:"https://res.cloudinary.com/trip-buddy/image/upload/v1628680402/trip%20buddy/sightseeing/tourist-attractions_5_vhlj9d.jpg",
   price: 500,
   stop_1: "Le Bois Cheri Restaurant - It is a must to come and experience the cuisine alongside its outstanding scenery when you are around the parts",
   stop_1_lat:"-20.4282",
   stop_1_long:"57.5223",
+  stop_1_img_url:"https://res.cloudinary.com/trip-buddy/image/upload/v1628830444/trip%20buddy/sightseeing/2747428_zNogcnlhgYmczu2LAw25QW9mOaee3yZx6hWrV_fHuZQ_txvxll.jpg",
   choice:"day",
   category:"sight-seeing"
 }
@@ -100,6 +113,7 @@ sea_cruise_trip1 =
    longitude:"57.6715",
    latitude:"-19.8863",
    duration:"17 hr",
+   image_url:"https://media-cdn.tripadvisor.com/media/photo-s/05/f6/2a/c5/catamaran-cruises-mauritius.jpg",
    price: 10900,
    choice:"night",
    category:"sea-cruising"
@@ -111,6 +125,7 @@ sea_cruise_trip2 =
    longitude:"57.6581",
    latitude:"-19.8750",
    duration:"4 hr",
+   image_url:"https://res.cloudinary.com/cruise-paradise/image/upload/v1628074128/cruise/IMG_1555-scaled_obvotj.jpg",
    price: 2500,
    choice:"day",
    category:"sea-cruising"
@@ -122,6 +137,7 @@ sea_cruise_trip3 =
    longitude:"57.7676",
    latitude:"-20.3991",
    duration:"7 hr",
+   image_url:"https://res.cloudinary.com/cruise-paradise/image/upload/v1628156894/cruise/overnight-catamaran-cruises-mauritius_1_dc9mg7.jpg",   
    price: 4500,
    choice:"day",
    category:"sea-cruising"
