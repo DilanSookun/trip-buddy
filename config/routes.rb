551c7c2a97b0214fa0fd3a8da818cecf92e97f21
Fabrice Madre <stepwiseltd@gmail.com>
2021-08-13 14:48:59 +0400

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'trips-choice', to: 'trips#trip_choice'
+  get 'trips-category', to: 'trips#trip_category'
   resources :trips do 
     resources :bookings, only: [:index, :show]
   end
